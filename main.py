8c72c996a7b79a42d395936a574242f8571b7ce7
Siddarth Narasimhan <sidarceus@gmail.com>
2021-06-27 21:24:35 -0400

    Uncommented main

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
-# from tkinter import *
-# from car import Car
-#
-# # Define Canvas and its Properties
-# width, height = 1400, 900
-# root = Tk()
-# root.title('RobotVision')
-# root.geometry(str(width) + 'x' + str(height))
-# my_canvas = Canvas(root, width=width, height=height, bg='white')
-# my_canvas.pack(padx=2, pady=2)
-#
-# # Create TurtleBot
-# bot = Car(root, my_canvas)
-# bot.build([float(40), float(40)])
-#
-# root.bind('<Right>', bot.move)
-# root.bind('<Up>', bot.rotateCCW)
-# root.bind('<Down>', bot.rotateCW)
-#
-# root.mainloop()
+from tkinter import *
+from car import Car
 
-from moviepy.editor import *
+# Define Canvas and its Properties
+width, height = 1400, 900
+root = Tk()
+root.title('RobotVision')
+root.geometry(str(width) + 'x' + str(height))
+my_canvas = Canvas(root, width=width, height=height, bg='white')
+my_canvas.pack(padx=2, pady=2)
 
-clip = (VideoFileClip("RobotVision1.mp4"))
-clip.write_gif("use_your_head.gif")
+# Create TurtleBot
+bot = Car(root, my_canvas)
+bot.build([float(40), float(40)])
+
+root.bind('<Right>', bot.move)
+root.bind('<Up>', bot.rotateCCW)
+root.bind('<Down>', bot.rotateCW)
+
+root.mainloop()
